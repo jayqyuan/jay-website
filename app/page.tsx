d862a3c52fb4f86f0d5e613a30161ff3b1d59124
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-10-04 18:43:23 -0400

    added tailwind and basic homepage styling

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,38 @@ export default async function Home() {
 	const projects = await getProjects();
 	return (
 		<main>
-			<div>
+			<div className='max-w-5xl mx-auto py-20'>
+				<h1 className='text-7xl font-extrabold'>
+					Hello, I&apos;m{" "}
+					<span className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+						Jay
+					</span>
+					!
+				</h1>
+        <p className="mt-3 text-xl text-gray-600">
+          Hi everyone, welcome to my page
+        </p>
+        <h2 className="mt-24 font-bold text-gray-700 text-3xl">
+          My Projects
+        </h2>
+        <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 				{projects.map((project: Project) => (
-					<div key={project._id}>{project.name}</div>
+					<div key={project._id} className="border border-gray-500 rounded-lg">
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={250}
+                height={100}
+                className="object-cover rounded-lg border border-gray-500"
+                />
+            )}
+            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+             {project.name} 
+            </div>
+          </div>
 				))}
+        </div>
 			</div>
 		</main>
 	);
