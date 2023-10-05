dc93ef58773cf50f4a7aefd00ecd1eb3c7b446fd
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-10-05 01:55:06 -0400

    base website completed

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,3 +1,4 @@
+import { getPages } from '@/sanity/sanity-utils'
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -10,18 +11,26 @@ export const metadata: Metadata = {
   description: 'Hi, my name is Jay! I am a software engineer who enjoys creating and developing products that make lives simplier and more meaningful',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pages = await getPages()
   return (
     <html lang="en">
       <body className='max-w-3xl mx-auto py-10'>
-        <header>
+        <header className='flex items-center justify-between'>
           <Link href='/' className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold'>
             Jay
           </Link>
+          <div className='flex items-center gap-3 text-sm text-gray-600'>
+            {pages.map((page)=>(
+              <Link key={page._id} href={`/${page.slug}`} className='hover:underline'>
+                {page.title}
+              </Link>
+            ))}
+          </div>
         </header>
         <main className='py-20'>{children}</main>
         </body>
