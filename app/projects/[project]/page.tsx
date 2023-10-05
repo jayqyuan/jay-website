5119d6e9d63999ef5dde735a8aecebe3855596f2
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-10-05 00:18:18 -0400

    updated slug page content/image css

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,14 +1,40 @@
-import { getProject } from "@/sanity/sanity-utils"
-
+import { getProject } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
-    params : {project: string}
-}
+	params: { project: string };
+};
 
-export default async function Project({params}: Props){
-    const slug = params.project
-    // this is using project because the file name is project
-    const project = await getProject(slug)
+export default async function Project({ params }: Props) {
+	const slug = params.project;
+	// this is using project because the file name is project
+	const project = await getProject(slug);
 
-    return <div>{project.name}e</div>
+	return (
+		<div className='max-w-3xl mx-auto py-20'>
+			<header className='flex items-center justify-between'>
+				<h1 className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-7xl font-extrabold'>
+					{project.name}
+				</h1>
+				<a
+					href={project.url}
+					title='View Project'
+					target='_blank'
+					rel='noopener noreferrer'
+                    className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+				>
+					View Project
+				</a>
+			</header>
+            {/* content section */}
+            <div className="text-lg text-gray-700 mt-5">
+                <PortableText value={project.content}/>
+            </div>
+
+            <Image src={project.image} alt={project.name} width={1920} height={1080} className="mt-10 border-2 border-gray-700 object-cover rounded-xl"/>
+
+            {/* image here */}
+		</div>
+	);
 }
