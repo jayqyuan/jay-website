4af5fb458761a047cf31b206e70f166ee91c75d8
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2023-12-06 22:47:23 -0500

    made the get in touch area look prettier by putting in a copy to clipboard for email address

--- app/(site)/components/Contact.tsx
+++ app/(site)/components/Contact.tsx
@@ -1,21 +1,56 @@
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import Link from 'next/link';
+"use client";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import Link from "next/link";
+import { useState } from "react";
+import { Tooltip } from "react-tooltip";
+
 export default function Contact() {
-    return(
-        <div className='mx-10'>
-            <h2 className='mt-24 font-bold text-gray-700 text-3xl dark:text-slate-200'>Get in touch</h2>
-            <h4 className='text-l px-10'>I am seeking an entry-level software engineer position, looking to work on a purposeful product that will be impactful to people.</h4>
-            <h3 className='mt-10 text-3xl font-bold max-w-3xl mx-auto'>Follow along: </h3>
-            <div className='my-10 max-w-3xl mx-auto'>
-                <a target="_blank" href='https://www.linkedin.com/in/jayqyuan/'>
-                    <LinkedInIcon className='h-14 w-14 mx-10 hover:scale-125 transition-all duration-500 cursor-pointer'/>
-                </a>
-                {''}
-                <a target="_blank" href='https://github.com/jayqyuan'>
-                    <GitHubIcon className='h-14 w-14 hover:scale-125 transition-all duration-500 cursor-pointer'/>
-                </a>
-            </div>
-        </div>
-    )
+	const [isCopied, setIsCopied] = useState(false);
+	const CopytoClipboard = async () => {
+		try {
+			await navigator.clipboard.writeText("jayqyuan@gmail.com");
+			setIsCopied(true);
+		} catch (error) {
+			console.error("Failed to copy", error);
+		}
+	};
+
+	return (
+		<div className='mx-10'>
+			<h2 className='mt-24 font-bold text-gray-700 text-3xl dark:text-slate-200'>
+				Get in touch
+			</h2>
+			<h4 className='text-l px-10'>
+				I am seeking an entry-level software engineer position, looking to work
+				on a purposeful product that will be impactful to people.
+			</h4>
+			<div className='my-10 max-w-3xl mx-auto flex justify-between'>
+				<a target='_blank' href='https://www.linkedin.com/in/jayqyuan/'>
+					<LinkedInIcon className='h-14 w-14 hover:scale-125 transition-all duration-500 cursor-pointer' />
+				</a>
+				{""}
+				<a target='_blank' href='https://github.com/jayqyuan'>
+					<GitHubIcon className='h-14 w-14 hover:scale-125 transition-all duration-500 cursor-pointer' />
+				</a>
+				<EmailIcon
+					onClick={CopytoClipboard}
+					className='h-14 w-14 hover:scale-125 transition-all duration-500 cursor-pointer'
+					data-tooltip-id='email-copied'
+					data-tooltip-content={isCopied?'Copied to clipboard':'Click to copy email!'}
+					data-tooltip-place='top'
+				/>
+				<Tooltip id="email-copied"/>
+				<style jsx global>{`
+					.email-copied {
+						background-color: #333;
+						color: #fff;
+						border-radius: 4px;
+						padding: 8px;
+					}
+				`}</style>
+			</div>
+		</div>
+	);
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,13 +8,13 @@ export default async function Home() {
 	const projects = await getProjects();
 	return (
 		<main>
-				<div id="about-me" className="h-screen">
-				<h1 className='text-7xl font-bold'>
+				<div id="about-me" className="h-screen m-3">
+				<h1 className='text-7xl sm:text-lg md:text-7xl font-bold py-5'>
 					<span className='bg-gradient-to-r from-orange-400 via-purple-600 to-slate-400 bg-clip-text text-transparent'>
 						Hello, I&apos;m{" "} Jay.
 					</span>
 				</h1>
-				<h1 className="text-4xl font-extra dark:text-slate-200 mt-1" >
+				<h1 className="text-4xl sm:text-xl font-extra dark:text-slate-200 mt-1" >
 					I like to develop cool projects.
 				</h1>
 				<p className='mt-3 text-l text-gray-600 dark:text-slate-400'>
