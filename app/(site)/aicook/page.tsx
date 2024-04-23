cf768845836ebde1c256fd6f92a48630751226e6
jayqyuan <118870651+jayqyuan@users.noreply.github.com>
2024-04-23 11:59:05 -0400

    fixed null error

--- app/(site)/aicook/page.tsx
+++ app/(site)/aicook/page.tsx
@@ -1,9 +1,19 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Page() {
 	const router = useRouter();
 
-	return router.push('https://ai-cookbook.vercel.app/');
+	useEffect(() => {
+		// Ensure code runs only on the client side
+		if (typeof window !== 'undefined') {
+			// Client-side code
+			router.push('https://ai-cookbook.vercel.app/');
+		}
+	}, []);
+
+	// Return null or a loading indicator while redirecting
+	return null;
 }
